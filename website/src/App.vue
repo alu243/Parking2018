17c9229abafeae84a98ecea63acb45b2f7110f39
bohao <bohao.chen@wegoluck.com>
2018-08-23 17:18:17 +0800

    First Check In

---
+++ website/src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app>
+    <v-navigation-drawer persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app value="abc" width="180">
+      <v-list>
+        <v-list-tile value="true" v-for="(item, i) in menus" :key="i">
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>web</v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>remove</v-icon>
+      </v-btn> -->
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+    <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed app>
+      <v-list>
+        <v-list-tile @click="right = !right">
+          <v-list-tile-action>
+            <v-icon>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			clipped: true,
+			drawer: true,
+			fixed: false,
+			menus: [
+				{ icon: 'image_search', title: '基本查詢' },
+				{ icon: 'library_books', title: '報表分析' },
+				{ icon: 'phonelink_setup', title: '資產管理' },
+				{ icon: 'schedule', title: '排程' },
+				{ icon: 'map', title: 'GIS分析' }
+			],
+			searchs: [
+				{ icon: '', title: '停車紀錄查詢' },
+				{ icon: '', title: '人員開單查詢' },
+				{ icon: '', title: '其他既有資料查詢' }
+			],
+			reports: [
+				{ icon: '', title: '開單統計' },
+				{ icon: '', title: '停車格周轉率' },
+				{ icon: '', title: '開單間隔排行' },
+				{ icon: '', title: '開單績效轉薪資' }
+			],
+			assets: [
+				{ icon: '', title: '設備管理' },
+				{ icon: '', title: '耗材管理' },
+				{ icon: '', title: '其他資產' },
+				{ icon: '', title: '人員管理' }
+			],
+			schedules: [
+				{ icon: '', title: '下載交通局資料' },
+				{ icon: '', title: '上傳交通局資料' },
+				{ icon: '', title: '下載google相片' },
+				{ icon: '', title: '刪除相片' }
+			],
+			gises: [
+				{ icon: '', title: '停車空格GIS查詢' },
+				{ icon: '', title: '人員開單軌跡' },
+				{ icon: '', title: '違規、贓車通報' }
+			],
+			miniVariant: false,
+			right: true,
+			rightDrawer: false,
+			title: '停管資訊系統'
+		};
+	}
+};
+</script>
