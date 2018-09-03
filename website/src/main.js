6937f2bdec5f4304df12b93e632468dd01602525
alu243 <alu243@gmail.com>
2018-09-04 00:25:15 +0800

    加入 vuex 以及 vue-router ，但是還不會動

--- website/src/main.js
+++ website/src/main.js
@@ -1,10 +1,30 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+// vue components
+import '@babel/polyfill';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Router from 'vue-router';
+import { sync } from 'vuex-router-sync';
+import './plugins/vuetify';
 
+// vue app components
+import App from './App.vue';
+//import VuexStore from './vuex/store';
+import { routers } from './router-config';
+
+Vue.use(Vuex);
+Vue.use(Router);
+
+//const store = new Vuex.Store(VuexStore); 
 Vue.config.productionTip = false
 
+const router = new Router({
+    routes,
+    mode: 'history',
+});
+//sync(store, router);
+
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    //store,
+    render: h => h(App)
+}).$mount('#app');
