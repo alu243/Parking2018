5b3b84205cd53742d4aad864a2cf0a45e137df5b
alu243 <alu243@gmail.com>
2018-11-15 02:09:06 +0800

    更新後端程式(下一次要拿掉 graphql

---
+++ Parking2018Api/Parking2018Api/Schema/BillQuery.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking2018Api.EF
+{
+    public class BillQuery : ObjectGraphType
+    {
+        public BillQuery(string query)
+        {
+            //Field<ListGraphType<>>
+            // 資料回傳型別
+            Field<ListGraphType<CaptionType>>(
+              "captions", // 查詢命令的名稱
+             arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id", Description = "Category id" }
+               ),// 查詢參數
+              resolve: context => { ... } // 準備回傳資料
+        }
+    }
+}
---
+++ Parking2018Api/Parking2018Api/Schema/Schemas.cs
@@ -0,0 +1,34 @@
+﻿using GraphQL.Net;
+using GraphQL.Types;
+using Parking2018Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking2018Api.EF
+{
+    public class Schemas
+    {
+        public GraphQLSchema BillSchema()
+        {
+            var schema = GraphQL<KHParkContext>.CreateDefaultSchema(() => new KHParkContext());
+            schema.AddType<M_BILL>().AddAllFields();
+            schema.AddListField("M_BILL_RECS", db => db.M_BILL_REC);
+            return schema;
+            //user.AddField(u => u.Id);
+            //user.AddField(u => u.Name);
+            //user.AddField(u => u.Account);
+            //user.AddField("totalUsers", (db, u) => db.Users.Count());
+            //user.AddField("accountPaid", (db, u) => u.Account.Paid);
+        }
+    }
+
+    //public class BillSchema : Schema
+    //{
+    //   public BillSchema(Func<Type, GraphType> resolveType) :base(resolveType)
+    //    {
+    //        Query = (BillQuery)resolveType(typeof(BillQuery));
+    //    }
+    //}
+}
